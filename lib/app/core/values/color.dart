baa74891aed855787b4b335c9db0ba317aed80f4
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2022-09-10 18:44:29 +0800

    update(color): add custom color scheme

--- lib/app/core/values/color.dart
+++ lib/app/core/values/color.dart
@@ -19,16 +19,84 @@ const kColorNeutral80 = Color(0xFF6C7072);
 /// kColorNeutral90 Heading, Subheading, Body Text
 const kColorNeutral90 = Color(0xFF090A0A);
 
+const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFF006397),
+  onPrimary: Color(0xFFFFFFFF),
+  primaryContainer: Color(0xFFCCE5FF),
+  onPrimaryContainer: Color(0xFF001D31),
+  secondary: Color(0xFF8E4E00),
+  onSecondary: Color(0xFFFFFFFF),
+  secondaryContainer: Color(0xFFFFDCC1),
+  onSecondaryContainer: Color(0xFF2E1500),
+  tertiary: Color(0xFF316B00),
+  onTertiary: Color(0xFFFFFFFF),
+  tertiaryContainer: Color(0xFFA2F967),
+  onTertiaryContainer: Color(0xFF0A2000),
+  error: Color(0xFFBA1A1A),
+  errorContainer: Color(0xFFFFDAD6),
+  onError: Color(0xFFFFFFFF),
+  onErrorContainer: Color(0xFF410002),
+  background: Color(0xFFFCFCFF),
+  onBackground: Color(0xFF001D32),
+  surface: Color(0xFFFCFCFF),
+  onSurface: Color(0xFF001D32),
+  surfaceVariant: Color(0xFFDEE3EB),
+  onSurfaceVariant: Color(0xFF42474E),
+  outline: Color(0xFF72787E),
+  onInverseSurface: Color(0xFFE7F2FF),
+  inverseSurface: Color(0xFF003351),
+  inversePrimary: Color(0xFF93CCFF),
+  shadow: Color(0xFF000000),
+  surfaceTint: Color(0xFF006397),
+);
 
-const kColorPrimary = Color(0xFF216DD9);
-const kColorPrimarySurface = Color(0xFFECF8FE);
-const kColorPrimaryBorder = Color(0xFFC4DAFA);
-const kColorPrimaryPressed = Color(0xFF054A80);
+const darkColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xFF93CCFF),
+  onPrimary: Color(0xFF003351),
+  primaryContainer: Color(0xFF004B73),
+  onPrimaryContainer: Color(0xFFCCE5FF),
+  secondary: Color(0xFFFFB778),
+  onSecondary: Color(0xFF4C2700),
+  secondaryContainer: Color(0xFF6C3A00),
+  onSecondaryContainer: Color(0xFFFFDCC1),
+  tertiary: Color(0xFF88DC4E),
+  onTertiary: Color(0xFF173800),
+  tertiaryContainer: Color(0xFF245100),
+  onTertiaryContainer: Color(0xFFA2F967),
+  error: Color(0xFFFFB4AB),
+  errorContainer: Color(0xFF93000A),
+  onError: Color(0xFF690005),
+  onErrorContainer: Color(0xFFFFDAD6),
+  background: Color(0xFF001D32),
+  onBackground: Color(0xFFCDE5FF),
+  surface: Color(0xFF001D32),
+  onSurface: Color(0xFFCDE5FF),
+  surfaceVariant: Color(0xFF42474E),
+  onSurfaceVariant: Color(0xFFC2C7CE),
+  outline: Color(0xFF8C9198),
+  onInverseSurface: Color(0xFF001D32),
+  inverseSurface: Color(0xFFCDE5FF),
+  inversePrimary: Color(0xFF006397),
+  shadow: Color(0xFF000000),
+  surfaceTint: Color(0xFF93CCFF),
+);
 
-const kColorSecondary = Color(0xFF216DD9);
-const kColorSecondarySurface = Color(0xFFECF8FE);
-const kColorSecondaryBorder = Color(0xFFC4DAFA);
-const kColorSecondaryPressed = Color(0xFF054A80);
+const kColorPrimary = Color(0xFF006397);
+const kColorPrimarySurface = Color(0xFFECF7FF);
+const kColorPrimaryBorder = Color(0xFF8FCDFF);
+const kColorPrimaryPressed = Color(0xFF235680);
+
+const kColorSecondary = Color(0xFFF8AB8D);
+const kColorSecondarySurface = Color(0xFFFEF7F4);
+const kColorSecondaryBorder = Color(0xFFFBCDBB);
+const kColorSecondaryPressed = Color(0xFF4A332A);
+
+const kColorTertiary = Color(0xFFB3DDC6);
+const kColorTertiarySurface = Color(0xFFF7FCF9);
+const kColorTertiaryBorder = Color(0xFFCAE7D7);
+const kColorTertiaryPressed = Color(0xFF6B8577);
 
 const kColorError = Color(0xFFFF5247);
 const kColorErrorSurface = Color(0xFFFEF3F2);
