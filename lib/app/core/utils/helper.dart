f41cf1e7c716fb7f21e6955ca0c5e5a482700236
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2022-09-10 18:46:39 +0800

    chore(helper): add format date helper

--- lib/app/core/utils/helper.dart
+++ lib/app/core/utils/helper.dart
@@ -1,3 +1,4 @@
+import 'package:intl/intl.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 Future<String> getAppVersion() async {
@@ -6,4 +7,18 @@ Future<String> getAppVersion() async {
   StringBuffer buffer = StringBuffer();
   buffer.writeAll(['v', version]);
   return buffer.toString();
+}
+
+String formatDate(String format, DateTime? date,
+    {String placeholder = '', bool showTimezone = true}) {
+  if (date == null) {
+    return placeholder;
+  }
+
+  if (format.contains('hh') || format.contains('mm') || format.contains('HH')) {
+    return showTimezone
+        ? '${DateFormat(format, 'id_ID').format(date)} ${date.timeZoneName}'
+        : DateFormat(format, 'id_ID').format(date);
+  }
+  return DateFormat(format, 'id_ID').format(date);
 }
