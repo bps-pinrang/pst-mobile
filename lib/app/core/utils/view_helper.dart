bd0efc9b8ccee9cf3bfcabd4ee309d7ddf114dd3
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2022-09-10 18:47:11 +0800

    chore(helper): add view helper

---
+++ lib/app/core/utils/view_helper.dart
@@ -0,0 +1,168 @@
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:pst_online/app/core/enums/app_animation.dart';
+import 'package:pst_online/app/core/enums/button_variant.dart';
+import 'package:pst_online/app/core/values/color.dart';
+import 'package:pst_online/app/core/values/size.dart';
+import 'package:pst_online/app/global_widgets/app_button.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+import 'package:sliding_sheet/sliding_sheet.dart';
+
+import '../enums/button_size.dart';
+
+Widget horizontalSpace(double spacing) => SizedBox(width: spacing);
+
+Widget verticalSpace(double spacing) => SizedBox(height: spacing);
+
+bool _checkIsAnythingOnScreen() {
+  return Get.isSnackbarOpen || Get.isBottomSheetOpen! || Get.isDialogOpen!;
+}
+
+Future<void> showConfirmationDialog(
+  BuildContext context, {
+  required String title,
+  required String content,
+  String? confirmLabel,
+  String? cancelLabel,
+  String? image,
+  required GestureTapCallback onConfirm,
+  GestureTapCallback? onCancel,
+}) async {
+  try {
+    if (_checkIsAnythingOnScreen()) Get.back();
+
+    Widget buildImage = Column(
+      children: [
+        Lottie.asset(
+          AppAnimation.warning.value,
+          width: Get.width * 0.3,
+          height: Get.width * 0.3,
+          fit: BoxFit.cover,
+        ),
+        verticalSpace(16),
+      ],
+    );
+
+    if (image != null) {
+      buildImage = Column(
+        children: [
+          ExtendedImage.asset(
+            image,
+            width: 160,
+            height: 160,
+          ),
+          verticalSpace(16),
+        ],
+      );
+    }
+
+    final theme = Theme.of(context);
+
+    return await Get.defaultDialog(
+      barrierDismissible: false,
+      title: '',
+      radius: 12,
+      titlePadding: EdgeInsets.zero,
+      content: Column(
+        children: [
+          buildImage,
+          Text(
+            title,
+            style: theme.textTheme.titleLarge,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+          ),
+          verticalSpace(8),
+          Padding(
+            padding: kPadding16H,
+            child: Text(
+              content,
+              style: theme.textTheme.caption,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+      contentPadding: kPadding16H8V.copyWith(
+        top: 0.0,
+      ),
+      actions: [
+        AppButton(
+          label: confirmLabel ?? 'Oke',
+          onPressed: onConfirm,
+          size: ButtonSize.small,
+          variant: ButtonVariant.primary,
+          isDense: true,
+        ),
+        AppButton(
+          label: cancelLabel ?? 'Batal',
+          onPressed: onCancel ?? Get.back,
+          size: ButtonSize.small,
+          variant: ButtonVariant.secondary,
+          isDense: true,
+        ),
+      ],
+    );
+  } catch (e) {
+    //
+  }
+}
+
+Future showBottomSheetDialog(
+  BuildContext context, {
+  Widget Function(BuildContext context, SheetState state)? headerBuilder,
+  Widget Function(BuildContext context, SheetState state)? footerBuilder,
+  required Widget content,
+}) async {
+  Widget Function(BuildContext context, SheetState state)? header =
+      headerBuilder;
+  final theme = Theme.of(context);
+  header ??= (_, __) => Padding(
+        padding: kPadding16V,
+        child: Container(
+          decoration: BoxDecoration(
+            color: theme.disabledColor.withOpacity(0.2),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          width: 38,
+          height: 8.0,
+        ),
+      );
+  return await showSlidingBottomSheet(
+    context,
+    resizeToAvoidBottomInset: true,
+    builder: (ctx) => SlidingSheetDialog(
+      cornerRadius: 16,
+      cornerRadiusOnFullscreen: 0,
+      avoidStatusBar: true,
+      headerBuilder: header,
+      snapSpec: const SnapSpec(
+        initialSnap: 0.8,
+        snappings: [0.4, 0.8, 1.0],
+      ),
+      builder: (_, state) => Material(
+        child: Padding(
+          padding: kPadding16,
+          child: content,
+        ),
+      ),
+      footerBuilder: footerBuilder,
+      duration: 300.milliseconds,
+    ),
+  );
+}
+
+Color checkFloatingLabelColor(
+    bool isFocus, bool isValid, AbstractControl formControl, ThemeData theme) {
+  if (formControl.pristine && !formControl.touched) {
+    return isFocus ? kColorPrimary : theme.colorScheme.onSurface;
+  }
+
+  if (!isValid && formControl.touched) {
+    return theme.errorColor;
+  }
+
+  return isFocus ? kColorPrimary : theme.colorScheme.onSurface;
+}
