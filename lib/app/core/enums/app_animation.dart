0b44b28f301438fa531ad41c1be2dbff7fb35cfa
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2022-09-11 15:41:55 +0800

    chore(enums): add new lottie animations to enum
    - add construction.json animation
    - add maintenance.json animation
    - add search.json animation

--- lib/app/core/enums/app_animation.dart
+++ lib/app/core/enums/app_animation.dart
@@ -7,6 +7,10 @@ enum AppAnimation {
     'assets/animations/book_idea.json',
     author: 'Md Fazle Hasan',
   ),
+  construction(
+    'assets/animations/construction.json',
+    author: 'Julia Polyakova',
+  ),
   loader('assets/animations/loader.json'),
   loader2('assets/animations/loader2.json'),
   loader3('assets/animations/loader3.json'),
@@ -14,11 +18,13 @@ enum AppAnimation {
     'assets/animations/login.json',
     author: 'Irfan Munawar',
   ),
+  maintenance('assets/animations/maintenance.json', author: 'vlk4graphic'),
   notFound(
     'assets/animations/not_found.json',
     author: 'Aakesh Deep',
   ),
   noInternet('assets/animations/no_internet.json', author: 'John Ocean'),
+  search('assets/animations/search.json', author: 'Akhil 3DI'),
   statistics(
     'assets/animations/statistics.json',
     author: 'Abdul Latif',
