63a57229487802449188325c61c7120cdd3665a0
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2022-09-12 09:32:24 +0800

    chore(ios): configure onesignal in ios

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Fajrian Aidil Pratama on 12/09/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
